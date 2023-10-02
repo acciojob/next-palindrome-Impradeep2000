4b367680c09ade7917e048103ed2bf6b0218438d
Pradeep Kumar <95734296+Impradeep2000@users.noreply.github.com>
2023-10-03 00:05:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function isPalindrome(n) {
+    return n.toString() === n.toString().split("").reverse().join("");
+  }
+
+  while (!isPalindrome(++num)) {}
+  return num;
 }
 
 const input = prompt("Enter a palindrome number");
